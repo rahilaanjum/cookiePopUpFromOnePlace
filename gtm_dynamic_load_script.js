a08bf89290af2b122e4862c55b2faa6364b7f3d3
kumar-tallapalli <77386924+kumar-tallapalli@users.noreply.github.com>
2022-05-31 17:17:01 +0530

    Updated hotjar content and removed GTM
    Updated hotjar content and removed GTM

--- gtm_dynamic_load_script.js
+++ gtm_dynamic_load_script.js
@@ -11,20 +11,12 @@ otSDKStubScript.setAttribute('data-domain-script','85f8227d-e015-4c62-ab96-ee1a0
 document.head.append(otSDKStubScript);
 //QA OneTrust Cookies Consent Notice end for infineon.com
 
-// Google Tag Manager
-const GTMSCRIPT_ID = "GTM-N8N7WR8";
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer',GTMSCRIPT_ID);
-// End Google Tag Manager
-
-window.onload = function() {
-	// Google Tag Manager (noscript)
-document.body.innerHTML = document.body.innerHTML + "<noscript><iframe src='https://www.googletagmanager.com/ns.html?id="+GTMSCRIPT_ID+"'"+
-				"height='0' width='0' style='display:none;visibility:hidden'></noscript>";
-// End Google Tag Manager (noscript)
-
-}
-
+//Hotjar Tracking Code as follows
+(function(h,o,t,j,a,r){
+	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2184213,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
