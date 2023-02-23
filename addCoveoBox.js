9d5f69f52efeb8ce63a9d2d60b6aec6bd7e36663
vivek-sisodiya <85992925+vivek-sisodiya@users.noreply.github.com>
2023-02-23 20:10:46 +0530

    Added Coveo js files as requested

---
+++ addCoveoBox.js
@@ -0,0 +1,7 @@
+	window.addEventListener('load', () => {
+ 	const title = document.querySelector('.title').textContent;
+ 	console.log('title: ', title);
+ 	CoveoInProduct.setContext({
+   	searchTerm: title,
+	 });
+	});
